91895c453c28e495824a00c34bce3b55cca84879
ghogho232 <holljjak00@gmail.com>
2023-09-04 20:32:45 +0900

    la

---
+++ syntax/object.js
@@ -0,0 +1,15 @@
+var members=['a','b','c'];
+console.log(members[1]);
+
+var i = 0;
+while(i < members.length){
+    console.log(members[i]);
+    i = i + 1;
+}
+var roles={
+    'programmer' : 'a',
+    'designer' : 'b',
+    'manager' : 'c'
+}
+
+console.log(roles.designer);
